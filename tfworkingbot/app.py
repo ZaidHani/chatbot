862ca51c499cf19423ed9a389d9eae6588ab68d1
Zaid Hani <99472607+ZaidHani@users.noreply.github.com>
2023-12-17 09:56:44 +0300

    Add files via upload

--- tfworkingbot/app.py
+++ tfworkingbot/app.py
@@ -3,11 +3,13 @@ import streamlit as st
 import tensorflow as tf
 
 # making a header and a title for the website
+st.title('Chatbot')
+st.markdown('This chatbot was made by using NLTK and Tensorflow, this is only a test chatbot.')
 
 #
 
-message = input("You: ")
+message = st.text_input("You: ")
 
 intents = pred_class(message, words, classes)
 result = get_response(intents, data)
-print(f'Bot: {result}')
+st.markdown(f'Bot: {result}')
